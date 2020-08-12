076812a933a528deb952a63782ec223d5780739c
alphadzingina <alphaadx6@gmail.com>
2020-08-12 04:20:42 +0100

    refactor component for reuse

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = ({ resource }) => {
+const useResources = (resource) => {
     const [resources, setResources] = useState([]);
-
+    
     const fetchResource = async (resource) => {
         const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
@@ -14,8 +14,14 @@ const ResourceList = ({ resource }) => {
         fetchResource(resource);
     }, [resource]);
 
+    return resources;
+}
+
+const ResourceList = ({ resource }) => {
+    const resources = useResources(resource);
+    
     return (
-    <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
+        <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
     );
 }
 
